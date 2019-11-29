23b8640247ca8ed71eae724ecfd90c54afc1cf12
Gavin <gavin.gosling@uoit.net>
2019-11-28 20:56:13 -0500

    Added Code for ColorManager and LightMixing Game

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -4,17 +4,66 @@ using UnityEngine;
 
 // Manages the colors with the scene and has interfaces allow for functions to change colors of objects
 
-public class ColorManager : MonoBehaviour
+public class CM
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void SetColorMaterialTransform(string colorString, Transform target)
     {
-        
+        Color color;
+        if (ColorUtility.TryParseHtmlString(colorString, out color))
+        {
+            target.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public static void SetColorMaterialTransform(Color color, Transform target)
     {
-        
+        target.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
     }
+
+    public static Color GetColorMaterialTransform(Transform target)
+    {
+        return target.GetComponent<MeshRenderer>().material.color;
+    }
+
+    public static string ConverToHex(Color color)
+    {
+        return "#" + ColorUtility.ToHtmlStringRGB(color);
+    }
+
+    public static Color ConvertToColor(string colorString)
+    {
+        Color color;
+        if (!colorString.Contains("#"))
+        {
+            colorString.Insert(0, "#");
+        }
+        if (ColorUtility.TryParseHtmlString(colorString, out color))
+        {
+            return color;
+        }
+        return color;
+    }
+
+    public void GetMaterialsFromObject(Transform Handpan, List<Material> Materials)
+    {
+        foreach (Transform trans in Handpan)
+        {
+            Materials.Add(trans.GetChild(0).GetComponent<MeshRenderer>().material);
+            Materials.Add(trans.GetChild(1).GetComponent<MeshRenderer>().material);
+        }
+    }
+
+    public static void DrawLineBetweenTwoVectors(Vector3 v1, Vector3 v2, Color color, Material lineMat, GameObject go, LineRenderer lineRenderer)
+    {
+        go.transform.position = v2;
+        lineRenderer.material = lineMat;
+        lineRenderer.SetPosition(0, v1);
+        lineRenderer.SetPosition(1, v2);
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+    }
+
 }
---
+++ Assets/Scripts/LightMixing.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMixing : MonoBehaviour
+{
+    public Material LineMaterial;
+    // Lasers
+    LineRenderer RedLaser;
+    LineRenderer GreenLaser;
+    LineRenderer BlueLaser;
+
+    // Mirror Gameobject
+    public GameObject Mirror;
+
+    // Laser GameObject
+    public GameObject RedLaserGO;
+    public GameObject GreenLaserGO;
+    public GameObject BlueLaserGO;
+    public GameObject OutputLaserGO;
+
+    // Current Color
+    public Color RedValue = Color.red;
+    public Color GreenValue = Color.green;
+    public Color BlueValue = Color.blue;
+    public Color OutputValue = new Color();
+
+    // Current Color String
+    public string RedValueStr = "";
+    public string GreenValueStr = "";
+    public string BlueValueStr = "";
+    public string OutputValueStr = "";
+
+    // Boolean
+    bool Red = false;
+    bool Green = false;
+    bool Blue = false;
+
+    // Variable
+    public int increment = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RedLaser = RedLaserGO.AddComponent<LineRenderer>();
+        GreenLaser = GreenLaserGO.AddComponent<LineRenderer>();
+        BlueLaser = BlueLaserGO.AddComponent<LineRenderer>();
+
+        CM.DrawLineBetweenTwoVectors(RedLaserGO.transform.position, Mirror.transform.position, RedValue, LineMaterial, RedLaserGO, RedLaser);
+        CM.DrawLineBetweenTwoVectors(BlueLaserGO.transform.position, Mirror.transform.position, BlueValue, LineMaterial, BlueLaserGO, BlueLaser);
+        CM.DrawLineBetweenTwoVectors(GreenLaserGO.transform.position, Mirror.transform.position, GreenValue, LineMaterial, GreenLaserGO, GreenLaser);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Red)
+        {
+
+        }
+
+        if (Green)
+        {
+
+        }
+
+        if (Blue)
+        {
+
+        }
+    }
+
+    public void Match()
+    {
+
+    }
+
+    public void Brighten()
+    {
+
+    }
+
+    public void Darken()
+    {
+
+    }
+
+    public void ChangeRed()
+    {
+
+    }
+
+    public void ChangeGreen()
+    {
+
+    }
+
+    public void ChangeBlue()
+    {
+
+    }
+}
