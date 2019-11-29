a1242c47ad71ef38c1529ee4e25a6d190676dc05
Gavin <gavin.gosling@uoit.net>
2019-11-29 00:20:17 -0500

    Almost Finished Light Mixing Game

--- Assets/Scripts/LightMixing.cs
+++ Assets/Scripts/LightMixing.cs
@@ -77,6 +77,7 @@ public class LightMixing : MonoBehaviour
         // Switch left
         if (Input.GetKeyDown(left))
         {
+            Debug.Log("Left");
             Red = false;
             Green = false;
             Blue = false;
@@ -104,6 +105,7 @@ public class LightMixing : MonoBehaviour
         // Switch Right
         if (Input.GetKeyDown(right))
         {
+            Debug.Log("Right");
             Red = false;
             Green = false;
             Blue = false;
@@ -130,11 +132,13 @@ public class LightMixing : MonoBehaviour
         {
             if (Input.GetKeyDown(up))
             {
+                Debug.Log("Up");
                 ChangeRed(true);
             }
 
             if (Input.GetKeyDown(down))
             {
+                Debug.Log("Down");
                 ChangeRed(false);
             }
             
@@ -142,12 +146,32 @@ public class LightMixing : MonoBehaviour
 
         if (Green)
         {
-            ChangeGreen();
+            if (Input.GetKeyDown(up))
+            {
+                Debug.Log("Up");
+                ChangeGreen(true);
+            }
+
+            if (Input.GetKeyDown(down))
+            {
+                Debug.Log("Down");
+                ChangeGreen(false);
+            }
         }
 
         if (Blue)
         {
-            ChangeBlue();
+            if (Input.GetKeyDown(up))
+            {
+                Debug.Log("Up");
+                ChangeBlue(true);
+            }
+
+            if (Input.GetKeyDown(down))
+            {
+                Debug.Log("Down");
+                ChangeBlue(false);
+            }
         }
     }
 
@@ -168,37 +192,141 @@ public class LightMixing : MonoBehaviour
 
     public void ChangeRed(bool up)
     {
-        string result = CM.ConvertToHex(OutputVal);
+        string result = CM.ConvertToHex(RedValue);
         string redstr = result[1].ToString() + result[2].ToString();
         int red = int.Parse(redstr, System.Globalization.NumberStyles.HexNumber);
+
         int newred = 0;
         if (up)
         {
-            newred = red + increment;
-        } else
-        {
-            newred = red - increment;
+            if(red+increment >= 255)
+            {
+                Debug.Log("Red at maximum");
+                return;
+            } else
+            {
+                newred = red + increment;
+            }
+            
+        } else {
+
+            if(red-increment<= 0)
+            {
+                Debug.Log("Red at zero");
+                return;
+            } else
+            {
+                newred = red - increment;
+            }
+            
         }
+
+        Debug.Log(newred);
+        string hexValue = "#" + newred.ToString("X") + "0000";
+        RedValue = CM.ConvertToColor(hexValue);
         
-        string hexValue = newred.ToString("X");
-        RedValue = new Color(newred, 0f, 0f);
         CM.DrawLineBetweenTwoVectors(RedLaserGO.transform.position, Mirror.transform.position, RedValue, LineMaterial, RedLaserGO, RedLaser, false);
+        ChangeOutput();
     }
 
-    public void ChangeGreen()
+    public void ChangeGreen(bool up)
     {
+        string result = CM.ConvertToHex(GreenValue);
+        string greenstr = result[3].ToString() + result[4].ToString();
+        int green = int.Parse(greenstr, System.Globalization.NumberStyles.HexNumber);
 
+        int newgreen = 0;
+        if (up)
+        {
+            if (green+increment >= 255)
+            {
+                Debug.Log("Green at maximum");
+                return;
+            }
+            else
+            {
+                newgreen = green + increment;
+            }
+
+        }
+        else
+        {
+
+            if (green-increment <= 0)
+            {
+                Debug.Log("Green at zero");
+                return;
+            }
+            else
+            {
+                newgreen = green - increment;
+            }
+
+        }
+
+        Debug.Log(newgreen);
+        string hexValue = "#00" + newgreen.ToString("X") + "00";
+        GreenValue = CM.ConvertToColor(hexValue);
+        CM.DrawLineBetweenTwoVectors(GreenLaserGO.transform.position, Mirror.transform.position, GreenValue, LineMaterial, GreenLaserGO, GreenLaser, false);
+        ChangeOutput();
     }
 
-    public void ChangeBlue()
+    public void ChangeBlue(bool up)
     {
+        string result = CM.ConvertToHex(BlueValue);
+        string bluestr = result[5].ToString() + result[6].ToString();
+        int blue = int.Parse(bluestr, System.Globalization.NumberStyles.HexNumber);
 
+        int newblue = 0;
+        if (up)
+        {
+            if (blue+increment >= 255)
+            {
+                Debug.Log("Blue at maximum");
+                return;
+            }
+            else
+            {
+                newblue = blue + increment;
+            }
+        }
+        else
+        {
+            if (blue-increment <= 0)
+            {
+                Debug.Log("Green at zero or will be below zero.");
+                return;
+            }
+            else
+            {
+                newblue = blue - increment;
+            }
+
+        }
+
+        Debug.Log(newblue);
+        string hexValue = "#0000" + newblue.ToString("X");
+        BlueValue = CM.ConvertToColor(hexValue);
+        Debug.Log(BlueValue);
+        CM.DrawLineBetweenTwoVectors(BlueLaserGO.transform.position, Mirror.transform.position, BlueValue, LineMaterial, BlueLaserGO, BlueLaser, true);
+        ChangeOutput();
+    }
+
+    public void ChangeOutput()
+    {
+        string red = CM.ConvertToHex(RedValue);
+        string green = CM.ConvertToHex(GreenValue);
+        string blue = CM.ConvertToHex(BlueValue);
+
+        string hexValue = "#" + red[1] + red[2] + green[3] + green[4] + blue[5] + blue[6];
+        OutputVal = CM.ConvertToColor(hexValue);
+        CM.DrawLineBetweenTwoVectors(Mirror.transform.position, OutputLaserGO.transform.position, OutputVal, LineMaterial, OutputLaserGO, OutputLaser, true);
     }
 
     public void PrintColor()
     {
         string result = CM.ConvertToHex(OutputVal);
-        Debug.Log("Color: " + result);
+        //Debug.Log("Color: " + result);
         string redstr = result[1].ToString() + result[2].ToString();
         string greenstr = result[3].ToString() + result[4].ToString();
         string bluestr = result[3].ToString() + result[4].ToString();
@@ -206,10 +334,10 @@ public class LightMixing : MonoBehaviour
         int red = int.Parse(redstr, System.Globalization.NumberStyles.HexNumber);
         int green = int.Parse(greenstr, System.Globalization.NumberStyles.HexNumber); 
         int blue = int.Parse(bluestr, System.Globalization.NumberStyles.HexNumber);
-        Debug.Log("Red: " + red + " Green: " + green + " Blue: " + blue);
+        //Debug.Log("Red: " + red + " Green: " + green + " Blue: " + blue);
 
         string hexValue = red.ToString("X");
-        Debug.Log("NewRed: " + hexValue);
+        //Debug.Log("NewRed: " + hexValue);
 
     }
 }
